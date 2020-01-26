2c4e159e33c5750d0d031f50881bf998f845c9fb
Joe Bateson <joe@joebateson.com>
2020-01-26 04:06:25 -0800

    comments

--- src/postcss/delete-rules.js
+++ src/postcss/delete-rules.js
@@ -4,9 +4,13 @@ module.exports = postcss.plugin("postcss-delete-rules", options => {
   const { selectorsToDelete } = options;
   return root => {
     root.walkRules(rule => {
+      // Get a list of the selectors we want to keep
       const newSelectors = rule.selectors.filter(
         selector => !selectorsToDelete.has(selector)
       );
+
+      // Remove the rule entirely if there are no
+      // remaining selectors
       if (newSelectors.length === 0) {
         rule.remove();
         return;
--- src/postcss/mangle.js
+++ src/postcss/mangle.js
@@ -5,12 +5,21 @@ module.exports = postcss.plugin("postcss-mangle-selectors", options => {
   return root => {
     root.walkRules(rule => {
       rule.selectors = rule.selectors.map(selector => {
+        // If we don't see the required "magic prefix", we
+        // know that this isn't a css module - just leave it alone
         if (!selector.startsWith("." + requiredPrefix)) {
           return selector;
         }
 
+        // The selector itself includes the initial .
         const oldSelector = selector.replace(".", "");
+
+        // We're dealing with a css module: but make sure that
+        // if we've already processed this selector, we re-use
+        // the name rather than making 2+ different ones
         const newSelector = cssClasses[oldSelector] || idGenerator();
+
+        // Update the storage object we were passed in from our caller
         cssClasses[oldSelector] = newSelector;
         return `.${newSelector}`;
       });
