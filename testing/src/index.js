cb45940391610f1e97c43518b3f35bd6d5664315
Joe Bateson <joe@joebateson.com>
2020-03-25 14:13:52 -0700

    Improve determinism and update contenthashes

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,6 +1,8 @@
 import styles from "./styles.module.css";
+import sharedStyles from "./shared.module.css";
 
 console.log(styles.used);
+console.log(sharedStyles["some-shared-class"]);
 
 function importFoo() {
   return import(/* webpackChunkName: "foo" */ "./foo");
@@ -8,3 +10,5 @@ function importFoo() {
 
 // Prevent tree-shaking
 window.importFoo = importFoo;
+
+window.alert(styles.someClass + styles.someClassWithASharedPrefix);
